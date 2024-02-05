6c717376588918c59d6b836b0fdc07d0810387e7
Andrei Scafi <andreiscafi@hotmail.com>
2024-02-05 09:11:19 -0300

    Displaying the tasks with javascript.

--- script.js
+++ script.js
@@ -46,5 +46,22 @@ taskForm.addEventListener('submit', (e) => {
     if (dataArrIndex === -1) {
         taskData.unshift(taskObj);
     }
+
+    // End of Displaying the tasks
+    taskData.forEach(({ id, title, date, description }) => {
+        tasksContainer.innerHTML += `
+        <div class="task" id="${id}"></div>
+        <p><strong>Title:</strong>${title}</p>
+        <p><strong>Date:</strong>${date}</p>
+        <p><strong>Description:</strong>${description}</p>
+        <button type="button" class="btn">Edit</button>
+        <button type="button" class="btn">Delete</button>
+        `
+    });
+
+    taskForm.classList.toggle('hidden');
+    // Displaying the tasks
+
 });
 // End of Event listeners
+
