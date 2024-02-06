c90024a7a36d1e16a829fe713b8d062f2416b835
Andrei Scafi <andreiscafi@hotmail.com>
2024-02-06 08:58:54 -0300

    Refactoring the function addOrUpdateTask with javascript.

--- script.js
+++ script.js
@@ -15,6 +15,48 @@ const taskData = [];
 let currentTask = {};
 // End of Variables
 
+// Task function refactoring
+
+const addOrUpdateTask = () => {
+    const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+
+    const taskObj = {
+        id: `${titleInput.value.toLowerCase().split(' ').join('-')}-${Date.now()}`,
+        title: titleInput.value,
+        date: dateInput.value,
+        description: descriptionInput.value,
+    }
+
+    if (dataArrIndex === -1) {
+        taskData.unshift(taskObj);
+    }
+
+    updateTaskContainer();
+    reset();
+}
+
+const updateTaskContainer = () => {
+
+    tasksContainer.innerHTML = '';
+
+    taskData.forEach(
+        ({ id, title, date, description }) => {
+            (tasksContainer.innerHTML += `
+            <div class="task" id="${id}">
+              <p><strong>Title:</strong> ${title}</p>
+              <p><strong>Date:</strong> ${date}</p>
+              <p><strong>Description:</strong> ${description}</p>
+              <button type="button" class="btn">Edit</button>
+              <button type="button" class="btn">Delete</button>
+            </div>
+          `)
+        }
+    );
+
+}
+
+// End of Task function refactoring
+
 //Reset function
 const reset = () => {
     titleInput.value = '';
@@ -53,32 +95,8 @@ discardBtn.addEventListener('click', () => {
 
 taskForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
-    const taskObj = {
-        id: `${titleInput.value.toLowerCase().split(' ').join('-')}-${Date.now()}`,
-        title: titleInput.value,
-        date: dateInput.value,
-        description: descriptionInput.value,
-    }
 
-    if (dataArrIndex === -1) {
-        taskData.unshift(taskObj);
-    }
-
-    // End of Displaying the tasks
-    taskData.forEach(({ id, title, date, description }) => {
-        tasksContainer.innerHTML += `
-        <div class="task" id="${id}"></div>
-        <p><strong>Title:</strong>${title}</p>
-        <p><strong>Date:</strong>${date}</p>
-        <p><strong>Description:</strong>${description}</p>
-        <button type="button" class="btn">Edit</button>
-        <button type="button" class="btn">Delete</button>
-        `
-    });
-
-    reset();
-    // Displaying the tasks
+    addOrUpdateTask();
 
 });
 // End of Event listeners
